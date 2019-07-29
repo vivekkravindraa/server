3258c007ea6940828a3d5acda966b611659798e3
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-07-30 02:58:36 +0530

    Bug: Fixed

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '708904043830-kbt1ars7ocv7chheqbe821rr1nb90bis.apps.googleusercontent.com',
+    googleClientSecret: '0HshGXNvRn1zVS4HEvvun8fs',
+    mongoURI: 'mongodb+srv://viveKsLoc:kusumaraVi4$@cluster0-a42ij.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'redacted'
+}
