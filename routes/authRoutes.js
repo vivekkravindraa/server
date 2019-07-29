ef91459a0eb02b720118f45e5f37e7132dabf111
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-07-30 02:52:11 +0530

    Basic Route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+    app.get('/', (req,res) => {
+        res.send({ 'Hi': 'Buddy!' })
+    });
+
     app.get('/auth/google', passport.authenticate('google',{
         scope: ['profile','email']
     }));
