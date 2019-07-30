9e921fa7396fa2af8e893d497639b2e3cd2a0a3a
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-07-30 18:11:19 +0530

    Async Await

--- services/passport.js
+++ services/passport.js
@@ -20,21 +20,18 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
+}, async (accessToken, refreshToken, profile, done) => {
 
     // console.log('accessToken', accessToken);
     // console.log('refreshToken', refreshToken);
     // console.log('profile', profile);
 
-    User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-        if(existingUser) {
-            done(null, existingUser);
-        } else {
-            new User({ googleId: profile.id }).save()
-            .then((user) => { done(null, user); })
-            .catch((e) => { console.log(e); })
-        }
-    })
-    .catch((e) => { console.log(e); })
+    const existingUser = await User.findOne({ googleId: profile.id })
+    if(existingUser) {
+        done(null, existingUser);
+    } else {
+        await new User({ googleId: profile.id }).save()
+        .then((user) => { done(null, user); })
+        .catch((e) => { console.log(e); })
+    }
 }));
