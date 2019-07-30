ed88f8a1c47d209ec7b4fc547087fd7f2abdc98c
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-07-30 17:45:13 +0530

    Proxy: Added

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>React App</h1>
-      <a href="/auth/google/"> Sign in with Google! </a>
+      <a href="http://localhost:5000/auth/google/"> Sign in with Google! </a>
     </div>
   );
 }
