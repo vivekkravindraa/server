9367618129fc21b3bb38339967d3f29e2634e732
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-07-30 16:44:38 +0530

    mlab Production

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <a href="/auth/google/"> Sign in with Google! </a>
+    </div>
+  );
+}
+
+export default App;
--- config/prod.js
+++ config/prod.js
@@ -6,3 +6,5 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 }
+
+// mongodb://vivekravindra4:kusumaraVi4$@ds239967.mlab.com:39967/clusterdb-prod
