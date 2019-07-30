0ca2c3ecc8f3a5f1719f14560edeee78071e96ea
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-07-30 13:13:35 +0530

    Adding Proxy

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
 
     // console.log('accessToken', accessToken);
