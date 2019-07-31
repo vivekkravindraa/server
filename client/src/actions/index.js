6c66ad0e53003380d7667f74bb1cc9cd7512648d
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-08-01 01:55:50 +0530

    clustery - client

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,9 +4,16 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('http://localhost:5000/api/current_user');
     dispatch({ type: FETCH_USER, payload: res.data });
-}
+};
 
 export const handleToken = (token) => async dispatch => {
     const res = await axios.post('/api/stripe', token);
-    dispatch({ type: FETCH_USER, payload: res.data })
-}
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values, history) => async dispatch => {
+    // return { type: 'submit_survey'}
+    const res = await axios.post('/api/surveys', values);
+    history.push('/surveys');
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Survey Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
+];
